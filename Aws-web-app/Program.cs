50048d360dc84e7923d055f58780cfa4e84885ba
Robin Minze <robminze@outlook.com>
2025-10-04 18:23:34 +0530

    go public

--- Aws-web-app/Program.cs
+++ Aws-web-app/Program.cs
@@ -32,6 +32,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-//app.Urls.Add("http://0.0.0.0:5000");
+app.Urls.Add("http://0.0.0.0:5000");
 
 app.Run();
